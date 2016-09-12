3157d3f6a603ca2f9556598064bd722276053895
krypton <154294300@qq.com>
2016-09-12 11:34:38 +0800

    xq-day1

---
+++ app/src/main/java/com/lanou/xuhaijia/doubanmix/xq/XiaoQing.java
@@ -0,0 +1,34 @@
+package com.lanou.xuhaijia.doubanmix.xq;/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  ` - `.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            佛祖保佑       永无BUG
+/**
+ * Created by 常久青 on 16/9/12.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+
+public class XiaoQing extends AppCompatActivity{
+    public static final String TAG = "TAG_XiaoQing";
+
+
+    //我是大神,请看
+
+}
